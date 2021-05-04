d1b304d808ecc532761feedaaad5761c707b1c3a
PedroAvila <minimainor_austin@hotmail.com>
2021-05-04 16:01:27 -0500

    Utilizando helpers display

---
+++ Models/ProductMetadata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace M0502.Models
+{
+    [MetadataType(typeof(ProductMetadata))]
+    public partial class Product
+    {
+        class ProductMetadata
+        {
+            [Display(Name = "Nombre del producto:")]
+            public object ProductName { get; set; }
+
+            [Display(Name = "Precio Unitario")]
+            [DisplayFormat(DataFormatString = "{0:C}")]
+            public object UnitPrice { get; set; }
+        }
+    }
+    
+}
