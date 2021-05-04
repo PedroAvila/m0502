2d9bb39370f2a5ab33214f750f60efbe60c41d9c
PedroAvila <minimainor_austin@hotmail.com>
2021-05-04 18:08:53 -0500

    Utilizando formularios para subir archivos

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -23,5 +23,35 @@ namespace M0502.Controllers
             var bytes = context.Categories.Where(c => c.CategoryID == id).Select(c => c.Picture).FirstOrDefault();
             return File(new MemoryStream(bytes, offset, bytes.Length - offset), "image/jpeg");
         }
+
+        public ActionResult UploadPhoto()
+        {
+            var category = new Category();
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult UploadPhoto2(Category newCategory, HttpPostedFileBase file)
+        {
+            ActionResult result = View(newCategory);
+
+            if (file != null && file.ContentLength > 0)
+            {
+                result = File(file.InputStream, file.ContentType);
+            }
+            return result;
+        }
+
+        [HttpPost]
+        public ActionResult UploadPhoto(Category newCategory, HttpPostedFileBase file)
+        {
+            ActionResult result = View(newCategory);
+
+            if (Request.Files.Count > 0 && Request.Files[0].ContentLength > 0)
+            {
+                result = File(Request.Files[0].InputStream, Request.Files[0].ContentType);
+            }
+            return result;
+        }
     }
 }
